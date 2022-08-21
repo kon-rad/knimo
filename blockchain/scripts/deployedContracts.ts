1f45f3aff91408342b275b1a9a4273d76064f91c
KaiStryker <kareemj.williams@gmail.com>
2022-08-20 20:27:04 -0500

    added addresses to deployContracts.ts

--- blockchain/scripts/deployedContracts.ts
+++ blockchain/scripts/deployedContracts.ts
@@ -3,6 +3,7 @@ export const deployedContracts = {
     freeCollectModule: "0x0BE6bD7092ee83D44a6eC1D949626FeE48caB30c",
     FollowerOnlyReferenceModule: "0x7Ea109eC988a0200A1F79Ae9b78590F92D357a16",
     perkStorage: "",
-    CollectNFT: "",
-    FollowNFT: ""
+    CollectNFT: "0x9417ffd1e38ef421ccd3dc5a90fdc46047afacc4",
+    FollowNFT: "0x1a2bb1bc90aa5716f5eb85fd1823338bd1b6f772",
+    GetMembership: ""
 }
